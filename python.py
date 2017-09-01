c3d9f5a75201f1ed77262402727cde9c785de107
rahulrkumar <nrkrahul@gmail.com>
2017-09-01 17:28:50 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,10 @@
+#Python programming is fun. isnt it ? 
+#!/usr/bin/python
+print " This is a test program to see the working of variable declaration " 
+
+str = 'hello world'
+
+print str
+
+print str[1:4]
+print str[1:]
